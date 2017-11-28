f3413d2891dd9c6fe6314390bc0a410ecfdd4c72
Gautam Dhameja <gautamdh@outlook.com>
2017-11-28 13:10:06 +0000

    Updated selections API

--- src/mashup/mtf-mashup.api/Models/MashupRequest.cs
+++ src/mashup/mtf-mashup.api/Models/MashupRequest.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
 
 namespace mtf_mashup.api.Models
 {
-    
     public class MashupRequest
     {
+        [JsonProperty("files")]
         public string[] Files { get; set; }
 
+        [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonProperty("passPhrase")]
         public string PassPhrase { get; set; }
     }
 }
